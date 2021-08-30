3dfb8f5e3b0d98c4bd1aee8bcaa91a03dc946c3b
andre <61792917+luuxis@users.noreply.github.com>
2021-08-31 00:07:06 +0200

    add download files

--- bootstrap/assets/c++/main.cpp
+++ bootstrap/assets/c++/main.cpp
@@ -1,15 +1,36 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Graphics.hpp>
+#include <curl/curl.h>
+#pragma warning(disable : 4996)
+
+
+void downloadFile(const char* url, const char* fname)
+{
+    CURL* curl;
+    FILE* fp;
+    CURLcode res;
+    curl = curl_easy_init();
+    
+    fp = fopen(fname, "wb");
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, FALSE);
+    res = curl_easy_perform(curl);
+    fclose(fp);
+}
 
 
 using namespace std;
 using namespace sf;
 
-int main()
+int main(int argc, char* argv[])
 {
+
     ShowWindow(GetConsoleWindow(), SW_HIDE);
     RenderWindow window(VideoMode(385, 110), "Craftdium Updater");
     Event event;
+    downloadFile("https://launcher.mojang.com/download/Minecraft.exe", "Minecraft.exe");
 
     while (window.isOpen())
     {
@@ -17,6 +38,7 @@ int main()
         window.display();
 
         window.waitEvent(event);
+        
         switch (event.type)
         {
         case Event::Closed:
